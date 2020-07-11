5abf58d9c84bdb55b34981b2264f7ec16f0ac46a
Robert Gardziński <gardzinskirobert@gmail.com>
2020-07-12 01:05:48 +0200

    Add js cod playerMove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let computerMove = `kamień`;
+let computerMove = `papier`;
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let computerMove = `kamień`;
+let playerMove = `papier`;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
