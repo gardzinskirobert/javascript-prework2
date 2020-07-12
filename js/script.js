8f1880f6542e0df7c220fdd6d4882c4054c0ca01
Robert Gardziński <gardzinskirobert@gmail.com>
2020-07-12 20:56:13 +0200

    Add let computerMove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let computerMove = `papier`;
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = `papier`;
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
