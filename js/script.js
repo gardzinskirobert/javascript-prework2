2aed16baf8147d9ad9ef81142a5fd8f412a6eb6a
Robert Gardziński <gardzinskirobert@gmail.com>
2020-07-15 00:03:47 +0200

    Add if else code

--- js/script.js
+++ js/script.js
@@ -32,3 +32,35 @@ if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove == 'papier' && playerMove == 'kamień') {
+	printMessage('Komputer wygrywa');
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce') {
+	printMessage('Ty wygrywasz!');
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier') {
+	printMessage('Komputer wygrywa');
+}
+else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+	printMessage('Ty wygrywasz!');
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+	printMessage('Komputer wygrywa');
+}
+
+else if (computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis');
+}
+else if (computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Remis');
+}
+else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Remis');
+}
+else (playerMove == 'nieznany ruch'){
+  	printMessage('Nieznany ruch');
+  }
